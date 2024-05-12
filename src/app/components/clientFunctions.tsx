e83ef39af0aa364d52f0253a22287bb9178d709f
the logical guy <146027539+Something-important@users.noreply.github.com>
2024-05-12 05:58:47 +0530

    added function to get exponent value

--- src/app/components/clientFunctions.tsx
+++ src/app/components/clientFunctions.tsx
@@ -1,5 +1,27 @@
 import { toHex, toBech32,fromBech32 } from "@cosmjs/encoding";
 import { assets, chains, ibc } from "chain-registry";
+
+export declare class Denom {
+  reference: string;
+  underlying?: Denom[] | undefined;
+  symbol: string;
+  decimals: number;
+  trace?: {
+      path: string;
+      base_denom: string;
+  };
+  ics20?: {
+      contract: string;
+      router: string;
+      channel: string;
+  };
+  constructor(reference: string, underlying?: Denom[] | undefined);
+  static from(string: string): Denom;
+  static from_path(port: string, channel: string, denom: string): Denom;
+  eq: (other: Denom) => boolean;
+  compare: (other: Denom) => number;
+  normalizedReference: () => string;
+}
 export function getAddress(chainName:string,address:string) {
   let filteredChains = chains.filter(({ chain_name }) => chain_name === chainName);
   let prefix = filteredChains[0].bech32_prefix;
@@ -18,3 +40,20 @@ export function minFee(chain:string){
   return{minFee,minFeeToken}
   
 } 
+
+export function getExponent(tokendenom: string, chain: string = "kujira") {
+    let TokenDecimals
+    if (tokendenom.includes("ibc/")) {
+        const denom = Denom.from(tokendenom);
+        const decimalPlacesToMove = denom.decimals;
+        TokenDecimals = Number(Math.pow(10, decimalPlacesToMove));
+    } else {
+        const assetList = assets.find(({ chain_name }) => chain_name === chain);
+        if (!assetList) throw new Error("Chain not found");
+        const filteredArray = assetList?.assets.find(obj => obj.base === tokendenom);
+        if (!filteredArray) throw new Error("Token not found");
+        const decimalPlacesToMove = filteredArray.denom_units[1].exponent;
+        TokenDecimals = Number(Math.pow(10, decimalPlacesToMove));
+    }
+    return TokenDecimals
+}
